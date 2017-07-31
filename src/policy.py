3853b5066e7e46fa96b8674e314806e294e5900a
pat-coady <3nik301>
2017-07-31 08:16:54 -0600

    comment typo

--- src/policy.py
+++ src/policy.py
@@ -66,7 +66,7 @@ class Policy(object):
         hid3_size = self.act_dim * 10  # 10 empirically determined
         hid2_size = int(np.sqrt(hid1_size * hid3_size))
         # heuristic to set learning rate based on NN size (tuned on 'Hopper-v1')
-        self.lr = 9e-4 / np.sqrt(hid2_size)  # 3e-4 empirically determined
+        self.lr = 9e-4 / np.sqrt(hid2_size)  # 9e-4 empirically determined
         # 3 hidden layers with tanh activations
         out = tf.layers.dense(self.obs_ph, hid1_size, tf.tanh,
                               kernel_initializer=tf.random_normal_initializer(
