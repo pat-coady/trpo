0534c485a7c7059ca5ed19a3ef8b09f1201fc554
pcoady <patrick.coady@analog.com>
2019-11-30 16:19:43 -0500

    more refactor

--- trpo/train.py
+++ trpo/train.py
@@ -306,7 +306,6 @@ def main(env_name, num_episodes, gamma, lam, kl_targ, batch_size, hid1_mult, pol
                 break
             killer.kill_now = False
     logger.close()
-    policy.close_sess()
 
 
 if __name__ == "__main__":
